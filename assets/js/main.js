7481bca9b312b8cb47652e62e3d0d32302e5552b
s.may.lee@gmail.com <s.may.lee@gmail.com>
2016-05-30 17:28:06 +0800

    add employer detail page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,11 @@
 $(function(){
     $('[data-toggle="tooltip"]').tooltip();
-    $('.footer').load('include-footer.html')
+    $('.footer').load('include-footer.html');
+
+
+    // for employer Gallery
+    $('.employerGallery .gllaeryPicList img').on('click',function(){
+    	var imgBigSrc = $(this).attr('data-src');
+    	$(this).parents('.employerGallery').find('.gllaeryPic img').attr('src', imgBigSrc)
+    })
 })
